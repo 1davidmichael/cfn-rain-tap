29f73835b64355e66ef909cb17126b4be0a559c9
David Michael <david.michael@meredith.com>
2019-10-30 12:56:14 -0500

    fix version

--- rain.rb
+++ rain.rb
@@ -6,15 +6,17 @@ class Rain < Formula
   if OS.mac?
     url 'https://github.com/aws-cloudformation/rain/releases/download/v0.7.0/rain-v0.7.0_osx-amd64.zip'
     sha256 '09d098602400f08299c86a8795f36217a74d20eb7a65806a99d06dfb138fdc50'
+    arch 'osx-amd64'
   elsif OS.linux?
     url 'https://github.com/aws-cloudformation/rain/releases/download/v0.7.0/rain-v0.7.0_linux-amd64.zip'
     sha256 'b3eb7308e806dbee3f5939d56f4c0ea1b1ef1100a3fddd8761b6272a9d2f806d'
+    arch 'linux-amd64'
   end
 
   depends_on :arch => :x86_64
 
   def install
-    bin.install "dist/rain-v#{version}_osx-amd64/"
+    bin.install "rain-v#{version}_#{arch}/"
   end
 
   test do
