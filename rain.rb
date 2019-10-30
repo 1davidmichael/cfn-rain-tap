d945c6fb230ba1d81badabe5ad564de696e9f2f4
David Michael <david.michael@meredith.com>
2019-10-30 13:21:26 -0500

    gonna fail

--- rain.rb
+++ rain.rb
@@ -6,17 +6,16 @@ class Rain < Formula
   if OS.mac?
     url 'https://github.com/aws-cloudformation/rain/releases/download/v0.7.0/rain-v0.7.0_osx-amd64.zip'
     sha256 '09d098602400f08299c86a8795f36217a74d20eb7a65806a99d06dfb138fdc50'
-    type 'osx-amd64'
   elsif OS.linux?
     url 'https://github.com/aws-cloudformation/rain/releases/download/v0.7.0/rain-v0.7.0_linux-amd64.zip'
     sha256 'b3eb7308e806dbee3f5939d56f4c0ea1b1ef1100a3fddd8761b6272a9d2f806d'
-    type 'linux-amd64'
   end
 
+  @@type = OS + "-amd64"
   depends_on :arch => :x86_64
 
   def install
-    bin.install "rain-v#{version}_#{OS}/"
+    bin.install "rain-v#{version}_#{@@type}/"
   end
 
   test do
